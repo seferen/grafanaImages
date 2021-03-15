c9e54bfdbc1cf6c8f9ca63abcfde1ecd9c689c0c
ZUBOV Ivan S <ivan.zubov@raiffeisen.ru>
2021-03-15 20:09:22 +0300

    Добавлен флаг для gui

--- main.go
+++ main.go
@@ -12,47 +12,53 @@ import (
 var (
 	configFile *string
 	prefix     *string
+	gui        *bool
 )
 
 func main() {
-	downChan := make(chan *grafana.FileUrl)
-	graf := grafana.Grafana{}
-	config.GetConfigFromFile(*configFile, &graf)
+	if *gui {
 
-	go func() {
-		log.Println("chan was started")
-		for {
-			select {
-			case x := <-downChan:
-				log.Println(">>>>>", x)
-				graf.DownloadFile(x)
+	} else {
+
+		downChan := make(chan *grafana.FileUrl)
+		graf := grafana.Grafana{}
+		config.GetConfigFromFile(*configFile, &graf)
+
+		go func() {
+			log.Println("chan was started")
+			for {
+				select {
+				case x := <-downChan:
+					log.Println(">>>>>", x)
+					graf.DownloadFile(x)
+
+				}
+			}
+		}()
+
+		dashs, err := graf.Search()
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		for _, dash := range dashs {
+			if _, b := graf.Config[dash.Title]; b {
+				log.Println(dash)
+				graf.Dashboards = append(graf.Dashboards, dash)
+				dashF, err := graf.GetDashboardByUid(dash.UID)
+				if err != nil {
+					log.Panicln(err)
+				}
+
+				for _, dash := range dashF.Dashboard.Panels {
+					dash.GetPanelIdWithGraph(&graf, dashF, downChan)
+				}
 
 			}
 		}
-	}()
-
-	dashs, err := graf.Search()
-	if err != nil {
-		log.Panicln(err)
+		close(downChan)
+		log.Println("Application was finished")
 	}
-
-	for _, dash := range dashs {
-		if _, b := graf.Config[dash.Title]; b {
-			log.Println(dash)
-			graf.Dashboards = append(graf.Dashboards, dash)
-			dashF, err := graf.GetDashboardByUid(dash.UID)
-			if err != nil {
-				log.Panicln(err)
-			}
-
-			for _, dash := range dashF.Dashboard.Panels {
-				dash.GetPanelIdWithGraph(&graf, dashF, downChan)
-			}
-
-		}
-	}
-	close(downChan)
-	log.Println("Application was finished")
 
 }
 
@@ -61,7 +67,7 @@ func init() {
 	configFile = flag.String("f", "config.json", "a file with configeration for app")
 	grafana.Dir = flag.String("dir", "result", "configure the path of a result directory where will download files of grapthics")
 	grafana.Prefix = flag.String("prefix", "", "prefix for downloading files")
-	// gui := flag.Bool("gui", false, "turn on gui as a server")
+	gui = flag.Bool("gui", false, "turn on gui as a server")
 	flag.Parse()
 
 	err := os.MkdirAll(*grafana.Dir, os.ModeAppend)
